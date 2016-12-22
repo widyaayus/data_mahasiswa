365feca2c66e241d0fb9f09e308b6e2be85de82c
bayumaulanaimran <bayu.maulana.imran@gmail.com>
2016-12-22 20:53:41 +0700

    salah nama variabel pada $connection

--- connection.php
+++ connection.php
@@ -3,5 +3,5 @@
 	$username = "root";
 	$password = "";	//password MySQL anda
 	$database = "data_mahasiswa"; //database anda
-	$connection = mysqli_connect($hostname, $username, $password, $database) or die("Unable to connect.");
+	$connection = mysqli_connect($namahost, $username, $password, $database) or die("Unable to connect.");
 ?>
