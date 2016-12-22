e0651c9a74f55d7add6aed8a50f53a9f175ad513
WIDYA\Widya <widyaayusuryaningtyas@gmail.com>
2016-12-22 20:28:15 +0700

    update ganti nama database

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 	$namahost = "localhost";
 	$username = "root";
 	$password = "";	//password MySQL anda
-	$database = "absensi"; //database anda
+	$database = "data_mahasiswa"; //database anda
 	mysql_connect($namahost,$username,$password) or die("Failed");
 	mysql_select_db($database) or die("Database not exist");
 ?>
